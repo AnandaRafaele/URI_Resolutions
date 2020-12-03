77f30eb9c33b2b3e4e48d2b7c9b6473cf100616b
AnandaRafaele <ananda_rafaele@hotmail.com>
2020-12-03 11:09:00 -0300

    1016

--- 1016.js
+++ 1016.js
@@ -1,11 +1,8 @@
 var input = require('fs').readFileSync('./stdin', 'utf8');
 var lines = input.split('\n');
 
-var car1 = 60
-var car2 = 90
+var km = parseInt(lines.shift());
 
-var km = parseInt(lines.shif())
+var minutos = km * 2;
 
-var minutos = (km / 2) * 60
-
-console.log(minutos + " minutos")
+console.log(`${minutos} minutos`);

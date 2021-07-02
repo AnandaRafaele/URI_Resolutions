2ac94e5427eb498acc579b39af2b1f9135dd6446
Ananda.Silva-ext <Ananda.Silva-ext@ab-Inbev.com>
2021-07-02 16:21:35 -0300

    1021

--- 1021.js
+++ 1021.js
@@ -1,17 +1,27 @@
-var input = require('fs').readFileSync('./stdin', 'utf8');
-var lines = input.split('\n');
+var input = require("fs").readFileSync("./stdin", "utf8");
+var lines = input.split("\n")[0].replace(",", ".");
 
-var valor = parseFloat(lines)
-var notas = [100, 50, 20, 10, 5, 2]
-var moedas = [1.00, 0.50, 0.25, 0.10, 0.05, 0.01]
+var notas = [100, 50, 20, 10, 5, 2];
+var moedas = [1.0, 0.5, 0.25, 0.1, 0.05, 0.01];
+var centavos = lines.split(".")[1] / 100;
+var reais = lines - centavos;
 
-// notas.map(nota => {
-//     let qnt = parseInt(valor / nota)
-//     console.log(`${qnt} nota(s) de R$ ${nota},00`)
-//     valor = valor % nota
-// })
+console.log("NOTAS:");
 
-moedas.map(moeda => {
-    let qnt = parseInt(valor - valor.toFixed(2) / moeda)
-    console.log(`${qnt.toFixed(2)} nota(s) de R$ ${moeda}`)
-})
+notas.map((nota) => {
+  let qnt = parseInt(reais / nota);
+  console.log(`${qnt} nota(s) de R$ ${nota.toFixed(2)}`);
+  reais = reais % nota;
+  if (reais === 1) {
+    centavos += 1;
+    reais = 0;
+  }
+});
+
+console.log("MOEDAS:");
+
+moedas.map((centavo) => {
+  let qnt = parseInt(centavos / centavo);
+  console.log(`${qnt} moeda(s) de R$ ${centavo.toFixed(2)}`);
+  centavos = (centavos % centavo).toFixed(2);
+});

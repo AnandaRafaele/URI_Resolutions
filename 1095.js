6b3d76adcc415987107a42f600fa2b211506a1e8
AnandaRafaele <ananda_rafaele@hotmail.com>
2021-07-12 11:18:55 -0300

    1095

---
+++ 1095.js
@@ -0,0 +1,11 @@
+var input = require("fs").readFileSync("./stdin", "utf8");
+var lines = input.split(/\r\n/);
+
+var i = 1,
+  j = 60;
+
+while (j >= 0) {
+  console.log(`I=${i} J=${j}`);
+  i += 3;
+  j -= 5;
+}
